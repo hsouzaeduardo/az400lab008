47840b7a83a519f084e9b4de771915a03344cfee
Henrique Souza <henrique.souza@gmail.com>
2021-06-06 14:59:10 -0300

    Commit inicial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace az400lab008.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
